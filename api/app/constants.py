0c20cefc8ea0023a2e6c127f39d50ab6f132e85b
Richard Si <sichard26@gmail.com>
2023-08-01 16:56:06 -0400

    [api] Reengineer how API configuration is done

--- api/app/constants.py
+++ api/app/constants.py
@@ -1,9 +1,42 @@
 import os
 from datetime import timedelta
 from pathlib import Path
-from typing import Final
+from typing import Any, Callable, Final, TypeVar
 
-ROOT_DIR: Final = Path(__file__).parent.parent.parent
+T = TypeVar("T")
+_MISSING: Final = object()
+
+
+def TimeDelta(value: str) -> timedelta:
+    kwargs = {}
+    for argument_string in value.replace(" ", "").split(","):
+        unit, value = argument_string.split("=")
+        kwargs[unit] = int(value)
+    return timedelta(**kwargs)
+
+
+def opt(name: str, type: Callable[[Any], T], default: Any = _MISSING) -> T:
+    """Read a value from the environment, after conversion.
+
+    The environment variable read is the option name uppercased with dashes
+    replaced with underscores and prefixed with TMC_. Example:
+
+        use-tls -> TMC_USE_TLS
+
+    If the envvar is missing, then the default is used. If a default is not
+    specified, a RuntimeError is raised.
+    """
+    name = "TMC_" + name.replace("-", "_").upper()
+    raw_value = os.getenv(name, default)
+    if raw_value is _MISSING:
+        raise RuntimeError(f"Missing environment variable: {name}")
+
+    try:
+        return type(raw_value)
+    except (TypeError, ValueError) as error:
+        context = f"|\n╰─> {error.__class__.__name__}: {error}"
+        raise RuntimeError(f"Invalid environment variable: {name}\n{context}")
+
 
 LOG_CONFIG: Final = {
     "version": 1,
@@ -26,23 +59,22 @@ LOG_CONFIG: Final = {
     },
 }
 
+# fmt: off
 # --- Database --- #
 
-if (_db_path := os.getenv("TMC_DATABASE_PATH")) is not None:
-    DATABASE_PATH = Path(_db_path)
-else:
-    DATABASE_PATH = Path(ROOT_DIR, "db.sqlite3")
+DATABASE_PATH: Final = opt("database", Path)
 
 # --- Authentication --- #
 
-ACCESS_TOKEN_LIFETIME: Final = timedelta(minutes=30)
-ALLOW_NEW_USERS: Final = bool(os.getenv("TMC_ALLOW_NEW_USERS", False))
-MAX_SESSIONS: Final = 50
-REFRESH_COOKIE_NAME: Final = "RefreshToken"
-SESSION_LIFETIME: Final = timedelta(days=1)
-SESSION_PURGE_DELTA: Final = timedelta(days=2)
+ALLOW_NEW_USERS: Final       = opt("allow-new-users",       bool,      default=False)
+REFRESH_COOKIE_NAME: Final   = opt("refresh-cookie-name",   str,       default="RefreshCookie")
+
+ACCESS_TOKEN_LIFETIME: Final = opt("access-token-lifetime", TimeDelta, default="minutes=30")
+SESSION_LIFETIME: Final      = opt("session-lifetime",      TimeDelta, default="days=1")
+SESSION_PURGE_DELTA: Final   = opt("session-purge-after",   TimeDelta, default="days=2")
+MAX_SESSIONS: Final          = opt("max-sessions",          int,       default=50)
 
 # --- Deployment --- #
 
-TLS_ENABLED: Final = bool(os.getenv("TMC_TLS_ENABLED", False))
-USE_UNIX_DOMAIN_SOCKET: Final = bool(os.getenv("TMC_USE_UNIX_DOMAIN_SOCKET", False))
+TLS_ENABLED: Final            = opt("tls",                bool, default=False)
+USE_UNIX_DOMAIN_SOCKET: Final = opt("unix-domain-socket", bool, default=False)
