cc7facbe24678233b600aeac2b7284f59a5d514f
Richard Si <sichard26@gmail.com>
2023-08-09 17:52:24 -0400

    [api] Refactor RateLimiter (and add auto-pruning)
    to avoid a disk exhaustion DOS.

--- api/app/utils.py
+++ api/app/utils.py
@@ -110,10 +110,47 @@ class RateLimiter:
     HOUR: Final = 60
     DAY: Final = 1440
 
+    class SQLiteBackend:
+        def __init__(self, db: sqlite3.Connection, max_windows: int = 5000) -> None:
+            self.db = db
+            self.max_windows = max_windows
+            self.commit = db.commit
+
+        def get_window(self, key: str, duration: int, limit: int) -> Optional[RateLimitWindow]:
+            cur = self.db.execute(
+                "SELECT rowid, value, expiry FROM ratelimits WHERE key = ? AND duration = ?;",
+                [key, duration]
+            )
+            if row := cur.fetchone():
+                return RateLimitWindow(duration=duration, limit=limit, value=row[1], expiry=row[2])
+            return None
+
+        def create_window(self, key: str, duration: int, limit: int, expiry: datetime) -> RateLimitWindow:
+            self.db.execute(
+                "INSERT INTO ratelimits(key, duration, value, expiry) VALUES(?, ?, ?, ?);",
+                [key, duration, 0, expiry]
+            )
+            return RateLimitWindow(duration=duration, limit=limit, value=0, expiry=expiry)
+
+        def update_window_key(self, key: str, by: int) -> None:
+            self.db.execute(
+                "UPDATE ratelimits SET value = value + ? WHERE key = ?;", [by, key]
+            )
+
+        def delete_window(self, key: str, duration: int) -> None:
+            self.db.execute(
+                "DELETE FROM ratelimits WHERE key = ? AND duration = ?;", [key, duration]
+            )
+
+        def prune(self) -> int:
+            count = self.db.execute("SELECT COUNT(*) FROM ratelimits;").fetchone()[0]
+            if (to_prune := count - self.max_windows) > 0:
+                self.db.execute("DELETE FROM ratelimits ORDER BY expiry LIMIT ?;", [to_prune])
+
     def __init__(self, key_prefix: str, limits: RateLimits, db: sqlite3.Connection) -> None:
         self.key_prefix = key_prefix + ":"
         self.limits = limits
-        self.db = db
+        self.backend = RateLimiter.SQLiteBackend(db)
 
     def update(self, key: str, by: int = 1) -> None:
         """Increment all windows.
@@ -122,11 +159,9 @@ class RateLimiter:
         """
         for duration in self.limits:
             self._get_or_create_window(self.key_prefix + key, duration)
-        with self.db:
-            self.db.execute(
-                "UPDATE ratelimits SET value = value + ? WHERE key = ?;",
-                [by, self.key_prefix + key]
-            )
+        self.backend.update_window_key(self.key_prefix + key, by)
+        self.backend.prune()
+        self.backend.commit()
 
     def should_block(self, key: str) -> bool:
         """Return whether at least one limit has been reached."""
@@ -140,7 +175,9 @@ class RateLimiter:
 
     def windows(self, key: str) -> Sequence[RateLimitWindow]:
         """Return rate limit windows."""
-        return [self._get_or_create_window(self.key_prefix + key, duration) for duration in self.limits]
+        wins = [self._get_or_create_window(self.key_prefix + key, duration) for duration in self.limits]
+        self.backend.commit()
+        return wins
 
     def reached_limits(self, key: str) -> Sequence[RateLimitWindow]:
         """Like windows() but returns only windows whose limit has been reached."""
@@ -148,22 +185,11 @@ class RateLimiter:
 
     def _get_or_create_window(self, key: str, duration: int) -> RateLimitWindow:
         limit = self.limits[duration]
-        cur = self.db.execute(
-            "SELECT rowid, value, expiry FROM ratelimits WHERE key = ? AND duration = ?;",
-            [key, duration]
-        )
-        if row := cur.fetchone():
-            stored = RateLimitWindow(duration=duration, limit=limit, value=row[1], expiry=row[2])
+        if stored := self.backend.get_window(key, duration, limit):
             if stored.expiry > utc_now():
                 return stored
-            cur.execute("DELETE FROM ratelimits WHERE rowid = ?;", [row["rowid"]])
 
-        default = RateLimitWindow(
-            duration=duration, limit=limit, value=0, expiry=utc_now() + timedelta(minutes=duration)
+            self.backend.delete_window(key, duration)
+        return self.backend.create_window(
+            key, duration, limit, expiry=utc_now() + timedelta(minutes=duration)
         )
-        cur.execute(
-            "INSERT INTO ratelimits(key, duration, value, expiry) VALUES(?, ?, ?, ?);",
-            [key, duration, 0, default.expiry]
-        )
-        self.db.commit()
-        return default
